a2cb3d32aa6ffdca2e2474694b4d8a613107f207
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-08-14 21:14:14 +0300

    Create UserRepo.java

---
+++ JavaTeamWork/UserRepo.java
@@ -0,0 +1,19 @@
+package com.datorium.Datorium.API.Repo;
+
+import com.datorium.Datorium.API.DTOs.User;
+
+import java.util.ArrayList;
+import java.util.List;
+public class UserRepo {
+
+    private ArrayList<User> users = new ArrayList<>();
+
+    public int add(User user){
+        users.add(user);
+        return users.size();
+    }
+    // New method to get all users
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
