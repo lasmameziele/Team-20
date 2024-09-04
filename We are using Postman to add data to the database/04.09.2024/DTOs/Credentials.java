5e04dbb5620d97370ac2464caf820be616debbee
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-04 20:19:55 +0300

    Create Credentials.java

---
+++ We are using Postman to add data to the database/04.09.2024/DTOs/Credentials.java
@@ -0,0 +1,9 @@
+package com.datorium.Datorium.API.DTOs;
+
+public class Credentials {
+    public static class UpdateUserDTO {
+        public User user;
+        public int userIndex;
+    }
+    }
+
