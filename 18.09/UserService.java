29c05558c56ac1aab9784f0c867bf80564147f5e
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-18 21:41:58 +0300

    Create UserService.java

---
+++ 18.09/UserService.java
@@ -0,0 +1,11 @@
+package com.datorium.Datorium.API;
+
+public class UserService {
+
+    public String randomData = "123456";
+    public String getFullName(String name, String surname){
+        return name + " " + surname;
+    }
+
+
+}
