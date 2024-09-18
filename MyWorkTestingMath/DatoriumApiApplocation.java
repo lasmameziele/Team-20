8920dc1a3afae8317c8dcf50cf47760a1e5653cc
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-18 22:01:09 +0300

    Update DatoriumApiApplocation.java

--- MyWorkTestingMath/DatoriumApiApplocation.java
+++ MyWorkTestingMath/DatoriumApiApplocation.java
@@ -14,8 +14,9 @@ public class DatoriumApiApplication {
 		var mathService = new MathService();
 
 		// Example usage of sum method
-		System.out.println("Sum of 50 and 40: " + mathService.sum(20, 75));  // Should return 95
-		System.out.println("Sum of 60 and 50: " + mathService.sum(70, 35));  // Should return 0
+		System.out.println("Sum of 20 and 75: " + mathService.sum(20, 75));  // Should return 95
+		System.out.println("Sum of 70 and 35: " + mathService.sum(70, 35));  // Should return 0
+		System.out.println("Sum of 99 and 2: " + mathService.sum(99, 2));	// Should still return 0
 		SpringApplication.run(DatoriumApiApplication.class, args);
 	}
 }
