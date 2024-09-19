81f4c76c6928a41608e5fe08ec691e508023c9d8
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-19 21:18:32 +0300

    Update NumberGuessingGame.java

--- UnitTests_NbrGuessingGame/NumberGuessingGame.java
+++ UnitTests_NbrGuessingGame/NumberGuessingGame.java
@@ -1,18 +1,18 @@
 package com.datorium.Datorium.API;
 
 public class NumberGuessingGame {
-    private int targetNumber;
+    public int myTargetNumber;
 
-    // Constructor to set the target number when the game starts
-    public NumberGuessingGame(int targetNumber) {
-        this.targetNumber = targetNumber;
+    // Constructor to set the target nbr
+    public NumberGuessingGame(int myTargetNumber) {
+        this.myTargetNumber = myTargetNumber;
     }
 
-    // Method to guess the number and return the result
+    // Method to guess the nbr and return the result
     public int guess(int number) {
-        if (number > targetNumber) {
+        if (number > myTargetNumber) {
             return 3; // Guess is too big
-        } else if (number < targetNumber) {
+        } else if (number < myTargetNumber) {
             return 2; // Guess is too small
         } else {
             return 1; // Guess is correct!
