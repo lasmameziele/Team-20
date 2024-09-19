70cf6e05f70e5e42580065f01778d6163e7cf49d
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-19 19:42:35 +0300

    Create DateService.java

---
+++ ContinueTesting/DateService.java
@@ -0,0 +1,12 @@
+package com.datorium.Datorium.API;
+
+import java.util.Calendar;
+
+public class DateService {
+
+    public boolean IsWeekend(Calendar calendar){
+        var currentDay = calendar.get(Calendar.DAY_OF_WEEK);
+
+        return (currentDay == Calendar.SATURDAY || currentDay == Calendar.SUNDAY);
+    }
+}
